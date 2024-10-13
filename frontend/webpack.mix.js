348c6d42262b2d80d21f3c4f6fcd518fe8a2d302
OidaTiftla <oidatiftla@oidatiftla.de>
2024-10-13 11:53:09 +0200

    Formatting only

--- frontend/webpack.mix.js
+++ frontend/webpack.mix.js
@@ -1,17 +1,16 @@
-let mix = require('laravel-mix');
-require('mix-tailwindcss');
+const mix = require("laravel-mix");
+require("mix-tailwindcss");
 
 mix
-    .js('src/js/app.js', 'js')
-    .sourceMaps()
-    // .sass('src/sass/app.scss', 'css')
-    .postCss('src/css/app.css', 'css')
-    .tailwind()
-    .copy('src/index.html', 'dist/index.html')
-    .setPublicPath('dist')
-    ;
+  .js("src/js/app.js", "js")
+  .sourceMaps()
+  // .sass('src/sass/app.scss', 'css')
+  .postCss("src/css/app.css", "css")
+  .tailwind()
+  .copy("src/index.html", "dist/index.html")
+  .setPublicPath("dist");
 
 mix.browserSync({
-    watch: true,
-    server: 'dist',
+  watch: true,
+  server: "dist",
 });
