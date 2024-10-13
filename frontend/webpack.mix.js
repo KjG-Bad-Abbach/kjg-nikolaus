1901b8b5355fe5500f0b831460225f09441050dd
OidaTiftla <oidatiftla@oidatiftla.de>
2024-10-13 11:08:33 +0200

    Add frontend files and configuration for tailwind-css and alpine.js

---
+++ frontend/webpack.mix.js
@@ -0,0 +1,17 @@
+let mix = require('laravel-mix');
+require('mix-tailwindcss');
+
+mix
+    .js('src/js/app.js', 'js')
+    .sourceMaps()
+    // .sass('src/sass/app.scss', 'css')
+    .postCss('src/css/app.css', 'css')
+    .tailwind()
+    .copy('src/index.html', 'dist/index.html')
+    .setPublicPath('dist')
+    ;
+
+mix.browserSync({
+    watch: true,
+    server: 'dist',
+});
