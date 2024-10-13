e7f05630ad8b9fa04c7d9b57adc8710944539e40
OidaTiftla <oidatiftla@oidatiftla.de>
2024-10-13 11:53:28 +0200

    Update index.html and webpack.mix.js for hashed file paths

--- frontend/webpack.mix.js
+++ frontend/webpack.mix.js
@@ -1,5 +1,17 @@
+const fs = require("fs");
 const mix = require("laravel-mix");
 require("mix-tailwindcss");
+
+function getIndexHtml() {
+  const hashes = JSON.parse(fs.readFileSync("dist/mix-manifest.json"));
+  // replace {{ mix('file') }} with the hashed file path
+  let content = fs.readFileSync("src/index.html").toString();
+  content = content.replace(
+    /{{ mix\('(.+?)'\) }}/g,
+    (_, file) => hashes[file] ?? file
+  );
+  return content;
+}
 
 mix
   .js("src/js/app.js", "js")
@@ -7,10 +19,19 @@ mix
   // .sass('src/sass/app.scss', 'css')
   .postCss("src/css/app.css", "css")
   .tailwind()
-  .copy("src/index.html", "dist/index.html")
-  .setPublicPath("dist");
+  .setPublicPath("dist")
+  .version();
 
 mix.browserSync({
   watch: true,
   server: "dist",
+  middleware: [
+    {
+      route: "/",
+      handle: (req, res, next) => {
+        res.write(getIndexHtml());
+        res.end();
+      },
+    },
+  ],
 });
