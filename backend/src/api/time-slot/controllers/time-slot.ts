06764177e033224f4adcb1fd2ffb2c913ac970ee
OidaTiftla <oidatiftla@oidatiftla.de>
2024-10-25 21:20:27 +0200

    feat: :rocket: introduce time-slot selection and storage and editing

--- backend/src/api/time-slot/controllers/time-slot.ts
+++ backend/src/api/time-slot/controllers/time-slot.ts
@@ -2,6 +2,47 @@
  * time-slot controller
  */
 
-import { factories } from '@strapi/strapi'
+import { factories } from "@strapi/strapi";
 
-export default factories.createCoreController('api::time-slot.time-slot');
+export default factories.createCoreController(
+  "api::time-slot.time-slot",
+  ({ strapi }) => ({
+    async find(ctx) {
+      // Call the default find method
+      const { data, meta } = await super.find(ctx);
+
+      // Add a calculated field to each time slot
+      const enhancedData = data.map((timeSlot) => {
+        if (timeSlot.bookings) {
+          const enhancedTimeSlot = {
+            ...timeSlot,
+            available_slots: timeSlot.max_bookings - timeSlot.bookings.length,
+          };
+          delete enhancedTimeSlot.bookings;
+          return enhancedTimeSlot;
+        }
+        return timeSlot;
+      });
+
+      // Return the enhanced data
+      return { data: enhancedData, meta };
+    },
+
+    async findOne(ctx) {
+      // Call the default findOne method
+      let { data, meta } = await super.findOne(ctx);
+
+      if (data.bookings) {
+        // Add a calculated field to the time slot
+        data = {
+          ...data,
+          available_slots: data.max_bookings - data.bookings.length,
+        };
+        delete data.bookings;
+      }
+
+      // Return the enhanced data
+      return { data, meta };
+    },
+  })
+);
