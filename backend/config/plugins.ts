d6272ec27a28ac805c5294452300851d723e74c0
OidaTiftla <oidatiftla@oidatiftla.de>
2024-10-29 08:53:30 +0100

    feat: :rocket: support sending mails

--- backend/config/plugins.ts
+++ backend/config/plugins.ts
@@ -1,1 +1,25 @@
-export default () => ({});
+export default ({ env }) => ({
+  email: {
+    config: {
+      provider: "nodemailer",
+      providerOptions: {
+        host: env("SMTP_HOST"),
+        port: env("SMTP_PORT"),
+        auth:
+          !env("SMTP_USERNAME") && !env("SMTP_PASSWORD")
+            ? undefined
+            : {
+                user: env("SMTP_USERNAME"),
+                pass: env("SMTP_PASSWORD"),
+              },
+        ignoreTLS: env("SMTP_IGNORE_TLS"),
+        connectionTimeout: env("SMTP_CONNECTION_TIMEOUT", 5000),
+        timeout: env("SMTP_TIMEOUT", 5000),
+      },
+      settings: {
+        defaultFrom: env("SMTP_DEFAULT_FROM"),
+        defaultReplyTo: env("SMTP_DEFAULT_REPLY_TO"),
+      },
+    },
+  },
+});
