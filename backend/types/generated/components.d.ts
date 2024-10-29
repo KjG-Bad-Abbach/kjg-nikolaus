8913c7cfabe66cca2df43c892a511162a2125add
OidaTiftla <oidatiftla@oidatiftla.de>
2024-10-29 09:16:53 +0100

    feat: :sparkles: allow to configure base url

--- backend/types/generated/components.d.ts
+++ backend/types/generated/components.d.ts
@@ -1,66 +1,4 @@
-import type { Struct, Schema } from '@strapi/strapi';
-
-export interface SharedContactPerson extends Struct.ComponentSchema {
-  collectionName: 'components_shared_contact_people';
-  info: {
-    displayName: 'Contact Person';
-    icon: 'user';
-    description: '';
-  };
-  attributes: {
-    first_name: Schema.Attribute.String &
-      Schema.Attribute.Required &
-      Schema.Attribute.SetMinMaxLength<{
-        maxLength: 50;
-      }>;
-    last_name: Schema.Attribute.String &
-      Schema.Attribute.Required &
-      Schema.Attribute.SetMinMaxLength<{
-        maxLength: 50;
-      }>;
-    phone_number: Schema.Attribute.String &
-      Schema.Attribute.SetMinMaxLength<{
-        maxLength: 50;
-      }>;
-    email: Schema.Attribute.Email &
-      Schema.Attribute.Required &
-      Schema.Attribute.SetMinMaxLength<{
-        maxLength: 100;
-      }>;
-  };
-}
-
-export interface SharedAddress extends Struct.ComponentSchema {
-  collectionName: 'components_shared_addresses';
-  info: {
-    displayName: 'Address';
-    icon: 'pinMap';
-    description: '';
-  };
-  attributes: {
-    street: Schema.Attribute.String &
-      Schema.Attribute.Required &
-      Schema.Attribute.SetMinMaxLength<{
-        maxLength: 100;
-      }>;
-    house_number: Schema.Attribute.String &
-      Schema.Attribute.Required &
-      Schema.Attribute.SetMinMaxLength<{
-        maxLength: 10;
-      }>;
-    zip_code: Schema.Attribute.String &
-      Schema.Attribute.Required &
-      Schema.Attribute.SetMinMaxLength<{
-        minLength: 5;
-        maxLength: 5;
-      }>;
-    place: Schema.Attribute.String &
-      Schema.Attribute.Required &
-      Schema.Attribute.SetMinMaxLength<{
-        maxLength: 100;
-      }>;
-  };
-}
+import type { Schema, Struct } from '@strapi/strapi';
 
 export interface BookingChild extends Struct.ComponentSchema {
   collectionName: 'components_booking_children';
@@ -78,12 +16,74 @@ export interface BookingChild extends Struct.ComponentSchema {
   };
 }
 
+export interface SharedAddress extends Struct.ComponentSchema {
+  collectionName: 'components_shared_addresses';
+  info: {
+    description: '';
+    displayName: 'Address';
+    icon: 'pinMap';
+  };
+  attributes: {
+    house_number: Schema.Attribute.String &
+      Schema.Attribute.Required &
+      Schema.Attribute.SetMinMaxLength<{
+        maxLength: 10;
+      }>;
+    place: Schema.Attribute.String &
+      Schema.Attribute.Required &
+      Schema.Attribute.SetMinMaxLength<{
+        maxLength: 100;
+      }>;
+    street: Schema.Attribute.String &
+      Schema.Attribute.Required &
+      Schema.Attribute.SetMinMaxLength<{
+        maxLength: 100;
+      }>;
+    zip_code: Schema.Attribute.String &
+      Schema.Attribute.Required &
+      Schema.Attribute.SetMinMaxLength<{
+        maxLength: 5;
+        minLength: 5;
+      }>;
+  };
+}
+
+export interface SharedContactPerson extends Struct.ComponentSchema {
+  collectionName: 'components_shared_contact_people';
+  info: {
+    description: '';
+    displayName: 'Contact Person';
+    icon: 'user';
+  };
+  attributes: {
+    email: Schema.Attribute.Email &
+      Schema.Attribute.Required &
+      Schema.Attribute.SetMinMaxLength<{
+        maxLength: 100;
+      }>;
+    first_name: Schema.Attribute.String &
+      Schema.Attribute.Required &
+      Schema.Attribute.SetMinMaxLength<{
+        maxLength: 50;
+      }>;
+    last_name: Schema.Attribute.String &
+      Schema.Attribute.Required &
+      Schema.Attribute.SetMinMaxLength<{
+        maxLength: 50;
+      }>;
+    phone_number: Schema.Attribute.String &
+      Schema.Attribute.SetMinMaxLength<{
+        maxLength: 50;
+      }>;
+  };
+}
+
 declare module '@strapi/strapi' {
   export module Public {
     export interface ComponentSchemas {
-      'shared.contact-person': SharedContactPerson;
-      'shared.address': SharedAddress;
       'booking.child': BookingChild;
+      'shared.address': SharedAddress;
+      'shared.contact-person': SharedContactPerson;
     }
   }
 }
